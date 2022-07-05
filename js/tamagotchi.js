22ce343bfc8b4e5e1aca6fa4ff796fdb373714c5
angeeg <angeleenasyse@gmail.com>
2022-07-05 16:46:19 -0700

    game object created, function to create tamagotchi made

--- js/tamagotchi.js
+++ js/tamagotchi.js
@@ -1,33 +1,49 @@
-// create a class for tamagotchi 
-class Tamagotchi{
-    constructor(name){
-        this.name = name 
-        this.hunger = 1
-        this.sleepiness = 1
-        this.boredom = 1
-        this.age = 0
-    }
-    // add method to feed tamagotchi, decrement hunger once fed, or increment hunger if not fed for a certain interval of time 
-    feedMe(){
-        console.log('Yummy thanks!')
-    }
+// create a class for tamagotchi
+class Tamagotchi {
+  constructor(name) {
+    this.name = name;
+    this.hunger = 5;
+    this.sleepiness = 5;
+    this.boredom = 5;
+    this.age = 0;
+  }
+  // add method to feed tamagotchi, decrement hunger once fed, or increment hunger if not fed for a certain interval of time
+  feedMe() {
+    console.log("Yummy thanks!");
+  }
 
-    // add method to put tamagotchi to sleep, decrement sleepiness once it goes to sleep, or increment sleepiness if awake for too long 
-    goToSleep(){
-        console.log('zzzzz')
-    }
+  // add method to put tamagotchi to sleep, decrement sleepiness once it goes to sleep, or increment sleepiness if awake for too long
+  goToSleep() {
+    console.log("zzzzz");
+  }
 
-    // add method to play with tamagotchi, decrement boredom once played with, increment boredom if neglected for 
-    play(){ 
-        console.log('this is so much fun!')
-    }
+  // add method to play with tamagotchi, decrement boredom once played with, increment boredom if neglected for
+  play() {
+    console.log("this is so much fun!");
+  }
 
-    // add method to increase age
-    ageUp(){
-        this.age += 1
-    }
+  // add method to increase age, have tamagotchi morph once reaching certain ages
+  // evolve at 10 and 20
+  ageUp() {
+    this.age += 1;
+  }
 }
 
-// create a class for the game itself 
+// create an object or class for the game itself
+const game = {
+  // instantiate tamagotchi
+  createTamagotchi: (name) => {
+    const tamagotchi = new Tamagotchi(name);
+    console.log(tamagotchi);
+  },
+};
+// create a global variable for a timer to be running throughout duration of the game
 
-// create a global variable for a timer to be running throughout duration of the game 
+// create function that starts the game - when the game starts the timer will start counting
+
+// create form to enter name for tamagotchi
+// create button to submit name
+
+// create buttons to eat, play, and sleep - turn lights off when sleeping
+
+game.createTamagotchi("ange");
