ca37fdfab5e3a1b0172c96de6c47e4e1547bc088
angeeg <angeleenasyse@gmail.com>
2022-07-05 19:43:35 -0700

    eat button decrements hunger value

--- js/tamagotchi.js
+++ js/tamagotchi.js
@@ -7,43 +7,63 @@ class Tamagotchi {
     this.boredom = 5;
     this.age = 0;
   }
+}
+// instantiate tamagotchi
+const tamagotchi = new Tamagotchi("Ange");
+console.log(tamagotchi);
+
+// create an object or class for the game itself
+const game = {
   // add method to feed tamagotchi, decrement hunger once fed, or increment hunger if not fed for a certain interval of time
-  feedMe() {
-    console.log("Yummy thanks!");
-  }
+  feedMe: () => {
+    tamagotchi.hunger -= 2;
+    console.log(`Hunger: ${tamagotchi.hunger}`);
+  },
 
   // add method to put tamagotchi to sleep, decrement sleepiness once it goes to sleep, or increment sleepiness if awake for too long
   goToSleep() {
     console.log("zzzzz");
-  }
+  },
 
   // add method to play with tamagotchi, decrement boredom once played with, increment boredom if neglected for
   play() {
     console.log("this is so much fun!");
-  }
+  },
 
   // add method to increase age, have tamagotchi morph once reaching certain ages
   // evolve at 10 and 20
   ageUp() {
     this.age += 1;
-  }
-}
-
-// create an object or class for the game itself
-const game = {
-  // instantiate tamagotchi
-  createTamagotchi: (name) => {
-    const tamagotchi = new Tamagotchi(name);
-    console.log(tamagotchi);
+    console.log(`${this.age}`);
   },
 };
+
 // create a global variable for a timer to be running throughout duration of the game
 
 // create function that starts the game - when the game starts the timer will start counting
 
 // create form to enter name for tamagotchi
+
 // create button to submit name
 
-// create buttons to eat, play, and sleep - turn lights off when sleeping
+// display stats on the window
+let hungerStat = document.querySelector("#hunger");
+hungerStat.innerHTML = `Hunger: ${tamagotchi.hunger}`;
 
-game.createTamagotchi("ange");
+let sleepinessStat = document.querySelector("#sleepiness");
+sleepinessStat.innerHTML = `Sleepiness: ${tamagotchi.sleepiness}`;
+
+let boredomStat = document.querySelector("#boredom");
+boredomStat.innerHTML = `Boredom: ${tamagotchi.boredom}`;
+
+let ageStat = document.querySelector("#age");
+ageStat.innerHTML = `Age: ${tamagotchi.age}`;
+
+
+
+// add function to buttons to eat, play, and sleep - turn lights off when sleeping
+let eatBtn = document.querySelector("#eat");
+eatBtn.addEventListener("click", game.feedMe );
+
+
+
