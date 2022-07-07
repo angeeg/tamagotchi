38db42b4820fe5fba84a45cadf2868b13b9e766c
angeeg <angeleenasyse@gmail.com>
2022-07-06 17:47:42 -0700

    feedme function working, timer added

--- js/tamagotchi.js
+++ js/tamagotchi.js
@@ -2,7 +2,7 @@
 class Tamagotchi {
   constructor(name) {
     this.name = name;
-    this.hunger = 5;
+    this.hunger = 6;
     this.sleepiness = 5;
     this.boredom = 5;
     this.age = 0;
@@ -18,29 +18,53 @@ const game = {
   feedMe: () => {
     tamagotchi.hunger -= 2;
     console.log(`Hunger: ${tamagotchi.hunger}`);
+    // if hunger reaches 0 alert "I'm too full, please feed later"
+    if(tamagotchi.hunger <= 0){
+      document.querySelector("#eat").disabled = true
+      alert(`${tamagotchi.name} seems to be very full, please feed later.`)
+    }
+    hungerStat.innerHTML = `Hunger: ${tamagotchi.hunger}`
+  },
+  // increase hunger every 15 seconds, if it reaches 10 game is over - tamagotchi dies
+  increaseHunger: () => {
+    timer
+    tamagotchi.hunger += 2 
+    hungerStat.innerHTML = `Hunger: ${tamagotchi.hunger}`
+    if(tamagotchi.hunger === 10){
+      clearInterval(timer)
+      console.log("timer has stopped")
+      alert(`Poor ${tamagotchi.name} was starved to death x_x`)
+    }
   },
 
   // add method to put tamagotchi to sleep, decrement sleepiness once it goes to sleep, or increment sleepiness if awake for too long
-  goToSleep() {
+  goToSleep: () => {
+
     console.log("zzzzz");
+    sleepinessStat.innerHTML = `Sleepiness: ${tamagotchi.sleepiness}`;
   },
 
   // add method to play with tamagotchi, decrement boredom once played with, increment boredom if neglected for
-  play() {
+  play: () => {
     console.log("this is so much fun!");
   },
 
   // add method to increase age, have tamagotchi morph once reaching certain ages
   // evolve at 10 and 20
-  ageUp() {
+  ageUp: () => {
     this.age += 1;
     console.log(`${this.age}`);
   },
 };
 
-// create a global variable for a timer to be running throughout duration of the game
-
 // create function that starts the game - when the game starts the timer will start counting
+const startGame = () => {
+  
+  console.log('the game has begun')
+}
+
+// create a global variable for a timer to be running throughout duration of the game
+const timer = setInterval(game.increaseHunger, 10000)
 
 // create form to enter name for tamagotchi
 
@@ -48,22 +72,26 @@ const game = {
 
 // display stats on the window
 let hungerStat = document.querySelector("#hunger");
-hungerStat.innerHTML = `Hunger: ${tamagotchi.hunger}`;
-
+hungerStat.innerHTML = `Hunger: ${tamagotchi.hunger}`
 let sleepinessStat = document.querySelector("#sleepiness");
 sleepinessStat.innerHTML = `Sleepiness: ${tamagotchi.sleepiness}`;
-
 let boredomStat = document.querySelector("#boredom");
 boredomStat.innerHTML = `Boredom: ${tamagotchi.boredom}`;
-
 let ageStat = document.querySelector("#age");
 ageStat.innerHTML = `Age: ${tamagotchi.age}`;
 
 
 
-// add function to buttons to eat, play, and sleep - turn lights off when sleeping
-let eatBtn = document.querySelector("#eat");
-eatBtn.addEventListener("click", game.feedMe );
+// add eventListeners to buttons to start, eat, play, and sleep - turn lights off when sleeping
+// const startBtn = document.querySelector("#start");
+// startBtn.addEventListener("click", startGame());
+const eatBtn = document.querySelector("#eat");
+eatBtn.addEventListener("click", game.feedMe);
+const playBtn = document.querySelector("#play");
+playBtn.addEventListener("click", game.play);
+const sleepBtn = document.querySelector("#sleep");
+sleepBtn.addEventListener("click", game.goToSleep);
+
 
 
 
