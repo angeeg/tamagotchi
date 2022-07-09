fee97643f4a49b27e7a94b95e49d0b72f75c1b18
angeeg <angeleenasyse@gmail.com>
2022-07-08 21:21:12 -0700

    aall stats increase every 5 seconds, age increases every 10 seconds

--- js/tamagotchi.js
+++ js/tamagotchi.js
@@ -31,37 +31,49 @@ const game = {
     hungerStat.innerHTML = `Hunger: ${tamagotchi.hunger}`
     if(tamagotchi.hunger === 10){
       clearInterval(timer)
-      console.log("timer has stopped")
       alert(`Poor ${tamagotchi.name} was starved to death x_x`)
     }
   },
 
   // add method to put tamagotchi to sleep, decrement sleepiness once it goes to sleep, or increment sleepiness if awake for too long
   goToSleep: () => {
-    console.log("zzzzz");
+    tamagotchi.sleepiness -= 1
     sleepinessStat.innerHTML = `Sleepiness: ${tamagotchi.sleepiness}`;
+    console.log("zzzzz");
   },
 
   increaseSleepiness: () => {
     tamagotchi.sleepiness += 1 
     sleepinessStat.innerHTML = `Sleepiness: ${tamagotchi.sleepiness}`
     if(tamagotchi.sleepiness === 10){
-      clearInterval(timer)
-      console.log("timer has stopped")
       alert(`Poor ${tamagotchi.name} has fainted from exhaustion x_x`)
+      stopTimer()
+      
     }
   },
 
   // add method to play with tamagotchi, decrement boredom once played with, increment boredom if neglected for
   play: () => {
+    tamagotchi.boredom -= 1
+    boredomStat.innerHTML = `boredom: ${tamagotchi.boredom}`
     console.log("this is so much fun!");
+  },
+
+  increaseBoredom: () => {
+    tamagotchi.boredom += 1 
+    boredomStat.innerHTML = `boredom: ${tamagotchi.boredom}`
+    if(tamagotchi.boredom === 10){
+      alert(`Poor ${tamagotchi.name} has died of boredom x_x`)
+      stopTimer()
+      
+    }
   },
 
   // add method to increase age, have tamagotchi morph once reaching certain ages
   // evolve at 10 and 20
   ageUp: () => {
-    this.age += 1;
-    console.log(`${this.age}`);
+    tamagotchi.age += 1;
+    ageStat.innerHTML = `Age: ${tamagotchi.age}`
   },
 };
 
@@ -72,18 +84,27 @@ const startGame = () => {
 }
 
 // create a global variable for a timer to be running throughout duration of the game
-const timer = () => {
+function timer() {
   let sec = 1
   setInterval(()=>{
     if(sec % 5 === 0){
       game.increaseHunger()
       game.increaseSleepiness()
+      game.increaseBoredom()
     }
-  
+    if(sec % 10 === 0){
+      game.ageUp()
+      console.log(tamagotchi.age)
+    }
+    
     document.querySelector("#timer").innerText = `${sec}`
     sec++
   }, 1000)
+}
 
+function stopTimer() {
+  clearInterval(timer)
+  console.log("timer has stopped")
 }
 
 // create form to enter name for tamagotchi
